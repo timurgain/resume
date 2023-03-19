efacd618ec91e030d3a7127aadde4a824aae4dd4
Timur Gain <timur.gain@gmail.com>
2023-03-19 18:57:22 +0300

    refactor: Resume data

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,27 +2,12 @@ import React from "react";
 import Resume from "./Resume/Resume";
 import MenuPopup from "./MenuPopup";
 import Footer from "./Footer";
-import openApi from "../utils/openApi";
 
 function App() {
-  const [resume, setResume] = React.useState({
-    article: "Loading...",
-    user: {
-      name: "Name",
-      surname: "Surname",
-      telegram: "link",
-      hard_skills: [{id: 1, order: 1, title: "Loading..."}],
-    },
-  });
-
-  React.useEffect(() => {
-    openApi.getResume(1).then((data) => setResume(data));
-  }, []);
-
   return (
     <>
       <MenuPopup />
-      <Resume {...resume} />
+      <Resume />
       <Footer />
     </>
   );
--- frontend/src/components/Resume/Resume.js
+++ frontend/src/components/Resume/Resume.js
@@ -1,19 +1,37 @@
+import React from "react";
+import openApi from "../../utils/openApi";
 import Bio from "./Bio";
 import HardSkills from "./HardSkills";
 import Education from "./Education";
 import Languages from "./Languages";
 import Article from "./Article";
 
-function Resume({ user, article, ...props }) {
+function Resume() {
+  const [resume, setResume] = React.useState({
+    article: "Loading...",
+    user: {
+      name: "Name",
+      surname: "Surname",
+      telegram: "link",
+      linkedin: "link",
+      github: "link",
+      hard_skills: [{ id: 1, order: 1, title: "Loading..." }],
+    },
+  });
+
+  React.useEffect(() => {
+    openApi.getResume(1).then((data) => setResume(data));
+  }, []);
+
   return (
     <>
       <header className="header">
-        <Bio {...user} />
-        <HardSkills hardSkills={user.hard_skills} />
+        <Bio {...resume.user} />
+        <HardSkills hardSkills={resume.user.hard_skills} />
         <Education />
         <Languages />
       </header>
-      <Article article={article} />
+      <Article article={resume.article} />
     </>
   );
 }
