4802ce4929d4da83a00cfd4f01faede80401ceb3
Timur Gain <timur.gain@gmail.com>
2023-04-25 12:51:07 +0300

    refactor: resume content

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,39 +1,62 @@
 function Article() {
   return (
-
     <main className="content">
+      <section className="section">
+        <h2 className="section__title">Experience</h2>
+        <p className="section__paragraph section__paragraph_layout_above-list">
+          Python developer, Business manager | alfabook.ru | 2019-2023
+        </p>
+        <ul className="list">
+          <li className="list__item">
+            Developed and maintained a web application for internal automation
+            of work processes with customers and suppliers, using Python, Flask,
+            SQLAlchemy, Redis Queue, HTML, Bootstrap, and Docker.
+          </li>
+          <li className="list__item">
+            Reduced the time for routine operations from ~20 clicks to 1-2
+            clicks, and enabled orders to be managed through the sales funnel
+            without the participation of an employee.
+          </li>
+          <li className="list__item">
+            Simplified work with external services such as CRM, cash register
+            equipment, Slack and Discord notifications, delivery services.
+          </li>
+          <li className="list__item">
+            Organized order picking through the web interface.
+          </li>
+        </ul>
 
-        <section className="section">
-          <h2 className="section__title">Experience in development</h2>
-          <p className="section__paragraph">
-            In September 2019 with two teammates began development the Flask web application for automating routine work processes.
-          </p>
-          <p className="section__paragraph">
-            After three months we released a minimum viable program, that was a great expansion of horizons and a breakthrough in efficiency - until recently, we did something in 20 clicks,
-            then just 1-2 clicks or completely without man participation. That time we felt like we were doing some kind of magic :)
-          </p>
-          <p className="section__paragraph">
-            For the project I attracted an experienced developer as a mentor, thanks to him we got acquainted with
-            Python, Flask, Redis Queue, PostgreSQL, Git and APIs of third-party services.
-          </p>
-          <p className="section__paragraph section__paragraph_layout_above-list">
-            Until spring 2021, we continued to develop the web application, as a result, business processes were digitized:
-          </p>
-          <ul className="list">
-            <li className="list__item">customer order processing, address processing, tagging, product availability check,</li>
-            <li className="list__item">creating a requirement for a supplier based on the balance, expectations, reserve at the supplier,</li>
-            <li className="list__item">receiving information about payments through the banking service via API,</li>
-            <li className="list__item">automatic notifications to customers via SMS and email services,</li>
-            <li className="list__item">notifications in Slack, then in Discord.</li>
-          </ul>
+        <p className="section__paragraph section__paragraph_layout_above-list">
+          Business manager | alfabook.ru | 2010-2023
+        </p>
+        <ul className="list">
+          <li className="list__item">
+            Managed the online store website, including filling and setting
+            tasks for a web studio.
+          </li>
+          <li className="list__item">
+            Promoted the store through Yandex.Direct, Google Ads, FB Ads, and
+            analytics.
+          </li>
+          <li className="list__item">
+            Managed pricing, stock of goods, and rules for working with
+            customers.
+          </li>
+          <li className="list__item">
+            Managed the work process for employees, including hiring and
+            temporary work.
+          </li>
+        </ul>
+        <p className="section__paragraph">
+          Starting in 2010, I co-founded and grew a offline store with a team of
+          three individuals into a successful online business with over 100,000
+          orders in the past five years and a team of up to 15 individuals.
+          Acquired valuable skills in management, internet marketing, and web
+          development through this experience.
+        </p>
+      </section>
 
-          <p className="section__paragraph">
-            Now we support the application and fix bugs.
-            Since September 2021 I have been devoting more time to training in specialized  programming courses.
-          </p>
-        </section>
-
-        <section className="section">
+      {/* <section className="section">
           <h2 className="section__title">Can do now</h2>
           <ul className="list">
             <li className="list__item">Django web-application</li>
@@ -43,33 +66,58 @@ function Article() {
             <li className="list__item">CI/CD with GitHub Actions or BitBucket Pipelines</li>
           </ul>
           <a className="section__link" href="https://github.com/timurgain" target="_blank" rel="noreferrer">Portfolio on GitHub</a>
-        </section>
+        </section> */}
 
-        <section className="section">
-          <h2 className="section__title">Universal skills</h2>
-          <p className="section__paragraph">
-            I combine development with a management position where I am responsible for
-            promotion, sales, an online store, motivation and organization of work processes for employees.
-          </p>
-          <p className="section__paragraph section__paragraph_layout_above-list">
-            This wide area of responsibility
-            and education with strict deadlines taught me universal skills:
-          </p>
-          <ul className="list">
-            <li className="list__item">Feeling a lack of knowledge motivates me to learn;</li>
-            <li className="list__item">Able to get information in documentation, lectures and communication with people;</li>
-            <li className="list__item">Understand how to divide a complex task into a set of simple ones;</li>
-            <li className="list__item">Self-discipline helps me complete tasks on time, especially in high workload;</li>
-            <li className="list__item">Cope with the flow of various tasks, build priorities, determine resources and deadlines;</li>
-            <li className="list__item">Understand the importance of communication and cooperation;</li>
-            <li className="list__item">I am prone to critical thinking, ready to new inputs and feedback, it leads to better solutions.</li>
-          </ul>
-        </section>
+      <section className="section">
+        <h2 className="section__title">Universal skills</h2>
+        <p className="section__paragraph section__paragraph_layout_above-list">
+          Throughout my career with wide area of responsibility and education
+          with strict deadlines I have developed a range of skills that are
+          applicable across various fields and industries, including:
+        </p>
+        <ul className="list">
+          <li className="list__item">
+            Skilled in problem-solving, from performing tasks to organizing
+            teams.
+          </li>
+          <li className="list__item">
+            Effective communication with colleagues and customers.
+          </li>
+          <li className="list__item">
+            Ability to break down complex tasks into simpler ones.
+          </li>
+          <li className="list__item">
+            Timely problem-solving and excellent self-discipline.
+          </li>
+          <li className="list__item">
+            Experienced in managing multiple tasks across different subjects.
+          </li>
+          <li className="list__item">
+            Open to new ideas, feedback, and input to drive better solutions.
+          </li>
+          <li className="list__item">Critical thinking skills.</li>
+        </ul>
+      </section>
 
-      </main>
-      
-  )
+      <section className="section">
+        <h2 className="section__title">About</h2>
+        <p className="section__paragraph">
+          I like technology, science, bikes, and exploring new ideas
+          through audiobooks. I can keep up a conversation about topics ranging
+          from computer games to planets, or discuss the fate of the Karamazov
+          brothers and Frank Algernon Cowperwood.
+        </p>
+        <p className="section__paragraph">
+          I am interested in web development projects that align with my values
+          and contribute to society.
+        </p>
+        <p className="section__paragraph">
+          I am open to relocation and am constantly seeking opportunities to
+          learn and grow.
+        </p>
+      </section>
+    </main>
+  );
 }
 
-
-export default Article
+export default Article;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,18 +12,24 @@ function Header() {
           </figcaption>
           <section aria-label="Contacts">
             <ul className="contacts">
-              <li className="contacts__item">
-                <a className="section__link" href="https://t.me/timur_gain"
-                  target="_blank" rel="noreferrer">Telegram</a>
-              </li>
-              <li className="contacts__item">
-                <a className="section__link" href="https://linkedin.com/in/timur-gainutdinov/"
-                  target="_blank" rel="noreferrer">LinkedIn</a>
-              </li>
+
               <li className="contacts__item">
                 <a className="section__link" href="https://github.com/timurgain"
                   target="_blank" rel="noreferrer">GitHub</a>
               </li>
+
+              {/* <li className="contacts__item">
+                <a className="section__link" href="https://t.me/timur_gain"
+                  target="_blank" rel="noreferrer">Telegram</a>
+              </li> */}
+
+              <li className="contacts__item">
+                <a className="section__link" href="https://linkedin.com/in/timur-gainutdinov/"
+                  target="_blank" rel="noreferrer">LinkedIn</a>
+              </li>
+
+
+
               <li className="contacts__item">timur.gain@gmail.com</li>
             </ul>
           </section>
@@ -35,44 +41,44 @@ function Header() {
           <ul className="hard-skills">
             <li className="hard-skills__item">Python</li>
             <li className="hard-skills__item">Django</li>
-            <li className="hard-skills__item">REST&nbsp;API</li>
-            <li className="hard-skills__item">PostgreSQL</li>
+            <li className="hard-skills__item">Flask</li>
+            <li className="hard-skills__item">API</li>
+            <li className="hard-skills__item">JS</li>
+            <li className="hard-skills__item">TS</li>
+            <li className="hard-skills__item">React.js</li>
+            <li className="hard-skills__item">Node.js</li>
+            <li className="hard-skills__item">SQL</li>
             <li className="hard-skills__item">Redis</li>
+            <li className="hard-skills__item">Mongo</li>
             <li className="hard-skills__item">Docker</li>
-            <li className="hard-skills__item">CSS</li>
-            <li className="hard-skills__item">HTML</li>
-            <li className="hard-skills__item">JavaScript</li>
-            <li className="hard-skills__item">Git</li>
           </ul>
         </section>
 
         <section className="section">
           <h2 className="section__title">Education</h2>
-          <p className="section__subtitle">I study at Yandex School of Data Analysis (Yandex.Practicum),
-            here're the subjects:</p>
+          <p className="section__subtitle">At Codecademy.com:</p>
           <ul className="education">
             <li className="education__item">
-              <p className="education__subject">Node.js, express.js</p>
+              <p className="education__subject">TypeScript, Redux.js</p>
+              <p className="education__date">June 2023</p>
+            </li>
+          </ul>
+          <p className="section__subtitle">At Yandex School of Data Analysis:</p>
+          <ul className="education">
+            <li className="education__item">
+              <p className="education__subject">Node.js, Express.js</p>
               <p className="education__date">April 2023</p>
             </li>
             <li className="education__item">
-              <p className="education__subject">Interfaces using React</p>
-              <p className="education__date">March 2023</p>
-            </li>
-            <li className="education__item">
-              <p className="education__subject">JavaScript, OOP, Async, API, Webpack</p>
+              <p className="education__subject">JavaScript, Async, React.js</p>
               <p className="education__date">January 2023</p>
             </li>
             <li className="education__item">
-              <p className="education__subject">JavaScript fundamenals</p>
-              <p className="education__date">November 2022</p>
-            </li>
-            <li className="education__item">
-              <p className="education__subject">HTML, CSS, BEM, Responsive layout</p>
+              <p className="education__subject">HTML, CSS, Responsive web layout</p>
               <p className="education__date">September 2022</p>
             </li>
             <li className="education__item">
-              <p className="education__subject">Docker, DevOps, CI/CD</p>
+              <p className="education__subject">Docker, Nginx, DevOps, CI/CD</p>
               <p className="education__date">May 2022</p>
             </li>
             <li className="education__item">
@@ -80,16 +86,19 @@ function Header() {
               <p className="education__date">March 2022</p>
             </li>
             <li className="education__item">
-              <p className="education__subject">Django REST framework and API</p>
-              <p className="education__date">January 2022</p>
+              <p className="education__subject">Django, Unittest, REST API</p>
+              <p className="education__date">December 2021</p>
             </li>
             <li className="education__item">
-              <p className="education__subject">Django, OOP, Unittest</p>
-              <p className="education__date">November 2021</p>
-            </li>
-            <li className="education__item">
-              <p className="education__subject">Python and SQL fundamentals</p>
+              <p className="education__subject">SQL, Python and OOP</p>
               <p className="education__date">September 2021</p>
+            </li>
+          </ul>
+          <p className="section__subtitle">At Moscow State Technical University</p>
+          <ul className="education">
+            <li className="education__item">
+              <p className="education__subject">Technical subjects, management</p>
+              <p className="education__date">Finished in 2010</p>
             </li>
           </ul>
         </section>
@@ -97,7 +106,7 @@ function Header() {
         <section className="section" aria-label="Languages">
           <h2 className="section__title">Languages</h2>
           <ul className="languages">
-            <li className="languages__item">English B2 (study)</li>
+            <li className="languages__item">English B2</li>
             <li className="languages__item">Russian native</li>
           </ul>
         </section>
